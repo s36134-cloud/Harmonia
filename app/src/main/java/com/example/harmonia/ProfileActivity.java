50e85705bcde95c5d70eacb8ea2657a808a29db8
s36134-cloud <s36134@haklaiph.org>
2026-01-07 11:07:08 +0200

    int

--- app/src/main/java/com/example/harmonia/ProfileActivity.java
+++ app/src/main/java/com/example/harmonia/ProfileActivity.java
@@ -1,6 +1,9 @@
 package com.example.harmonia;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class ProfileActivity extends AppCompatActivity {
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        auth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
@@ -19,6 +25,20 @@ public class ProfileActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button feedButton = findViewById(R.id.signout_button);
+        feedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+
+                Intent intent=new Intent(ProfileActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+
         });
     }
 }
