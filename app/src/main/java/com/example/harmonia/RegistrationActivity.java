2decb934c172548df4273f1a60024ba7aa1f436d
s36134-cloud <s36134@haklaiph.org>
2026-01-07 09:39:41 +0200

    int

--- app/src/main/java/com/example/harmonia/RegistrationActivity.java
+++ app/src/main/java/com/example/harmonia/RegistrationActivity.java
@@ -1,6 +1,12 @@
 package com.example.harmonia;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +14,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.harmonia.utils.RegistrationManager;
+
 public class RegistrationActivity extends AppCompatActivity {
 
+    private EditText emailEditText;
+    private EditText passwordEditText ;
+    private static final String TAG = "RegistrationActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +31,41 @@ public class RegistrationActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        emailEditText=findViewById(R.id.email_edit_text);
+        passwordEditText=findViewById(R.id.password_edit_text);
+
+        Button registerButton = findViewById(R.id.registar_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerButtonClick();
+
+            }
+        });
+
     }
+
+    private void registerButtonClick() {
+        Log.d(TAG, "Register button clicked");
+
+        RegistrationManager registrationManager = new RegistrationManager(RegistrationActivity.this);
+        registrationManager.startRegistration(
+                emailEditText.getText().toString(),
+                passwordEditText.getText().toString(),
+                new RegistrationManager.OnResultCallback(){
+                    @Override
+                    public void onResult(boolean success, String message) {
+                        if (success) {
+                            Toast.makeText(RegistrationActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(RegistrationActivity.this, "Registration failed: " + message, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
 }
