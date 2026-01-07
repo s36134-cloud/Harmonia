5799479e6dd5e45aa9069909e5b9659d5b863e3c
s36134-cloud <s36134@haklaiph.org>
2026-01-07 10:54:24 +0200

    int

--- app/src/main/java/com/example/harmonia/LoginActivity.java
+++ app/src/main/java/com/example/harmonia/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,7 +24,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        auth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
@@ -33,9 +34,17 @@ public class LoginActivity extends AppCompatActivity {
             return insets;
         });
 
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            Log.i("LoginActivity", "User already signed in, navigating to FeedActivity");
+            Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
         TextView tvRegister = findViewById(R.id.dont_have_account);
-
+        emailEditText=findViewById(R.id.email_edit_text);
+        passwordEditText=findViewById(R.id.password_edit_text);
         tvRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,6 +53,17 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        Button loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // invoke the performLogin method
+                performLogin();
+            }
+        });
+
+
     }
 
     private void performLogin() {
